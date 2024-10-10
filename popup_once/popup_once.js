af47fc2ee94e5e498d16374943668e886afa38de
Chayan Kumawat <130827975+Notchayan@users.noreply.github.com>
2024-10-10 14:56:54 +0530

    Refactored the repository structure

--- popup_once/popup_once.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const visitBtn = document.getElementById('visitBtn');
-  visitBtn.addEventListener('click', function() {
-    chrome.tabs.create({ url: 'https://pingala.iitk.ac.in/IITK-0/login' });
-    window.close();
-  });
-});
